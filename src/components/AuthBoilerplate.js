0424bbd829359df3db3eac0140f55fcbcf29ff8d
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-03-10 05:05:32 +0300

    moving on with the shape of the nav bar and the sidebar

--- src/components/AuthBoilerplate.js
+++ src/components/AuthBoilerplate.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Segment, Button } from 'semantic-ui-react';
+import { Segment, Button, Dropdown, Menu, Grid, Image, Container, Card , Label, Input, Icon} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 export default class AuthBoilerplate extends Component {
   state = {
     userIsSigned: false,
     user: null,
+    activeItem: 'home',
   }
 
   componentDidMount() {
@@ -16,12 +17,86 @@ export default class AuthBoilerplate extends Component {
     console.log(this.state)
   }
 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
+
     if (this.state.userIsSigned) {
       return (
         <div>
-          {this.props.children}
-          <Button onClick={this.handleLogout}>Logout</Button>
+          <Menu attached size='tiny'>
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+            <Menu.Item
+              name='messages'
+              active={activeItem === 'messages'}
+              onClick={this.handleItemClick}
+            />
+
+            <Menu.Menu position='right'>
+              <Menu.Item>
+                King Abdullah Hospital
+              </Menu.Item>
+
+              <Menu.Item>
+                <Button color="red">Sign Out</Button>
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+
+
+
+            <Grid centered padded>
+              <Grid.Column width={3}>
+        
+                <Grid.Row>
+                  <Card fluid>
+                    <Image src='/doctor-profile-picture.jpg' />
+                    <Card.Content>
+                      <Card.Header>Matthew</Card.Header>
+                      <Card.Meta>
+                        <span className='date'>Joined in 2015</span>
+                      </Card.Meta>
+                      <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <a>
+                        <Icon name='user' />
+                        22 Friends
+                      </a>
+                    </Card.Content>
+                  </Card>
+                </Grid.Row>
+
+
+                <Grid.Row style={{marginTop: '30px'}}>
+                  <Menu fluid vertical>
+                    <Menu.Item name='inbox' active={activeItem === 'inbox'} onClick={this.handleItemClick}>
+                      <Label color='teal'>1</Label>
+                      Inbox
+                    </Menu.Item>
+
+                    <Menu.Item name='spam' active={activeItem === 'spam'} onClick={this.handleItemClick}>
+                      <Label>51</Label>
+                      Spam
+                    </Menu.Item>
+
+                    <Menu.Item name='updates' active={activeItem === 'updates'} onClick={this.handleItemClick}>
+                      <Label>1</Label>
+                      Updates
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Input icon='search' placeholder='Search mail...' />
+                    </Menu.Item>
+                  </Menu>
+                </Grid.Row>
+
+              </Grid.Column>
+              <Grid.Column width={12}>
+                {this.props.children}
+                <Button onClick={this.handleLogout}>Logout</Button>
+              </Grid.Column>
+            </Grid>
         </div>
       );
     } 
