57e074ff1a4e771d3730c1ffef63fed30d29ed44
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-03-13 05:07:34 +0300

    Add comments

--- src/data/types.js
+++ src/data/types.js
@@ -1,3 +1,8 @@
+// This file contains the list of staff types for a hospital. Depending of the staff type, the appropriate page is rendered.
+// Every type containes the following info: name, canCreate, permissions.
+// canCreate indicated weather or not the type can create a new medical record.
+// permissions lists the different permissions for the type.
+
 import permissions from './permissions';
 
 let types = {
