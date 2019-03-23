ce269c3362cc28952cd76c556eb7b30e8c1751bc
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-03-23 17:53:42 +0300

    creating the surgeries page

--- src/components/pages/MedicalRecord.js
+++ src/components/pages/MedicalRecord.js
@@ -6,89 +6,97 @@ import { Card, Table, Grid, Segment, Container, Label, Button } from "semantic-u
 import { withRouter, Link } from 'react-router-dom'
 
 class MedicalRecord extends Component {
-  state = {
-    id: 0
-  };
+    state = {
+        id: 0
+    };
 
-  render() {
+    render() {
 
-    const cardPlaceholderPath = '/card-placeholder.png'
+        const cardPlaceholderPath = '/card-placeholder.png'
 
-    return (
-      <AuthBoilerplate>
-        <Container padded="true" style={{ padding: "20px" }}>
-            <Segment>
-            <h3>Someone's Medical Record</h3>
-            </Segment>
-            
-            <Segment>
-                <Grid columns={2} divided>
-                    <Grid.Column>
-                        <Table celled>
-                            <Table.Body>
-                                <Table.Row>
-                                    <Table.Cell active>Name</Table.Cell>
-                                    <Table.Cell>Cell</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell active>DOB</Table.Cell>
-                                    <Table.Cell>Cell</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell active>Phone</Table.Cell>
-                                    <Table.Cell>Cell</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell active>Gender</Table.Cell>
-                                    <Table.Cell>Cell</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell active>Blood Type</Table.Cell>
-                                    <Table.Cell>Cell</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell active>National ID</Table.Cell>
-                                    <Table.Cell>Cell</Table.Cell>
-                                </Table.Row>
-                            </Table.Body>
-                        </Table>
-                    </Grid.Column>
+        return (
+            <AuthBoilerplate>
+                <Container padded="true" style={{ padding: "20px" }}>
+                    <Segment>
+                        <h3>Someone's Medical Record</h3>
+                    </Segment>
 
-                    <Grid.Column>
-                    <Table celled>
-                            <Table.Body>
-                                <Table.Row>
-                                    <Table.Cell active>Hospital Name</Table.Cell>
-                                    <Table.Cell>Cell</Table.Cell>
-                                </Table.Row>
-                            </Table.Body>
-                        </Table>
-                    </Grid.Column>
-                </Grid>
-            </Segment>
-            <Segment>
-                <Card.Group>
-                    <Link style={{ padding: '10px' }} to={`/${this.state.id}/diagnoses`} >
-                        <Card color='orange' raised header='Diognosis' image={cardPlaceholderPath} />
-                    </Link>
-                    <Link style={{ padding: '10px' }} to={`/${this.state.id}/surgeries`} >
-                        <Card color='red' raised header='Surgeries' image={cardPlaceholderPath} />
-                    </Link>
-                    <Link style={{ padding: '10px' }} to={`/${this.state.id}/labTests`} >
-                        <Card color='green' raised header='Lab Tests' image={cardPlaceholderPath} />
-                    </Link>
-                    <Link style={{ padding: '10px' }} to={`/${this.state.id}/radiologyScans`} >
-                        <Card color='yellow' raised header='Radiology Scans' image={cardPlaceholderPath} />
-                    </Link>
-                    <Link style={{ padding: '10px' }} to={`/${this.state.id}/medicinePrescriptions`} >
-                        <Card color='blue' raised header='Medicine Prescriptions' image={cardPlaceholderPath} />
-                    </Link>
-                </Card.Group>
-            </Segment>
-        </Container>
-      </AuthBoilerplate>
-    );
-  }
+                    <Segment>
+                        <Grid columns={2} divided>
+                            <Grid.Column>
+                                <Table celled>
+                                    <Table.Body>
+                                        <Table.Row>
+                                            <Table.Cell active>Name</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell active>DOB</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell active>Phone</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell active>Gender</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell active>Blood Type</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell active>National ID</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                    </Table.Body>
+                                </Table>
+                            </Grid.Column>
+
+                            <Grid.Column>
+                                <Table celled>
+                                    <Table.Body>
+                                        <Table.Row>
+                                            <Table.Cell active>Hospital Name</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell active>Created At</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                        <Table.Row>
+                                            <Table.Cell active>Emergency Contact</Table.Cell>
+                                            <Table.Cell>Cell</Table.Cell>
+                                        </Table.Row>
+                                    </Table.Body>
+                                </Table>
+                            </Grid.Column>
+                        </Grid>
+                    </Segment>
+                    <Segment>
+                        <Card.Group>
+                            <Link style={{ padding: '10px' }} to={`/${this.state.id}/diagnoses`} >
+                                <Card color='orange' raised header='Diognosis' image={cardPlaceholderPath} />
+                            </Link>
+                            <Link style={{ padding: '10px' }} to={`/${this.state.id}/surgeries`} >
+                                <Card color='red' raised header='Surgeries' image={cardPlaceholderPath} />
+                            </Link>
+                            <Link style={{ padding: '10px' }} to={`/${this.state.id}/labTests`} >
+                                <Card color='green' raised header='Lab Tests' image={cardPlaceholderPath} />
+                            </Link>
+                            <Link style={{ padding: '10px' }} to={`/${this.state.id}/radiologyScans`} >
+                                <Card color='yellow' raised header='Radiology Scans' image={cardPlaceholderPath} />
+                            </Link>
+                            <Link style={{ padding: '10px' }} to={`/${this.state.id}/medicinePrescriptions`} >
+                                <Card color='blue' raised header='Medicine Prescriptions' image={cardPlaceholderPath} />
+                            </Link>
+                        </Card.Group>
+                    </Segment>
+                </Container>
+            </AuthBoilerplate>
+        );
+    }
 }
 
 export default withRouter(MedicalRecord);
--- src/components/pages/Surgeries.js
+++ src/components/pages/Surgeries.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import AuthBoilerplate from '../AuthBoilerplate'
-import { Segment, Container, Form, Message, Label, Button, Radio } from 'semantic-ui-react'
+import { Segment, Container, Card, Table, Form, Message, Label, Button, Radio, Image } from 'semantic-ui-react'
+import { SSL_OP_NETSCAPE_CHALLENGE_BUG } from 'constants';
 
 export default class Home extends Component {
   state = {
@@ -17,10 +18,115 @@ export default class Home extends Component {
   }
 
   render() {
+    const surgeries = [
+      {
+        surgeryName: 'Coronary artery bypass',
+        doctor: 'dr. Khaled Saud',
+        date: 973904461,
+        isMedicalError: false,
+        isCorrectionFor: null,
+        hospitalName: 'King Khaled Hospital',
+        fileHash: '1230874612983',
+        duration: 60,
+        blockchainAddress: '0x012ax9sa897099sd98aada9889x8s9dsa08dee'
+      },
+      {
+        surgeryName: 'Debridement of wound, burn, or infection',
+        doctor: 'dr. Khaled Saud',
+        date: 973904461,
+        isMedicalError: false,
+        isCorrectionFor: null,
+        hospitalName: 'King Khaled Hospital',
+        fileHash: '1230874612983',
+        duration: 60,
+        blockchainAddress: '0x012ax9sa897099sd98aada9889x8s9dsa08dee'
+      },
+      {
+        surgeryName: 'Dilation and curettage (D&C)',
+        doctor: 'dr. Khaled Saud',
+        date: 973904461,
+        isMedicalError: false,
+        isCorrectionFor: null,
+        hospitalName: 'King Khaled Hospital',
+        fileHash: '1230874612983',
+        duration: 60,
+        blockchainAddress: '0x012ax9sa897099sd98aada9889x8s9dsa08dee'
+      },
+      {
+        surgeryName: 'Low back pain surgery',
+        doctor: 'dr. Khaled Saud',
+        date: 973904461,
+        isMedicalError: false,
+        isCorrectionFor: null,
+        hospitalName: 'King Khaled Hospital',
+        fileHash: '1230874612983',
+        duration: 60,
+        blockchainAddress: '0x012ax9sa897099sd98aada9889x8s9dsa08dee'
+      },
+
+    ]
+
+    let surgeriesCards = surgeries.map((surgery) => {
+      return (
+        <Card fluid color='red' header='Option 1' style={{margin: "30px"}}>
+            <Table celled>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell width="3" active>Surgery Name</Table.Cell>
+                        <Table.Cell width="4">{surgery.surgeryName}</Table.Cell>
+                        <Table.Cell width="3" active>Surgery Date</Table.Cell>
+                        <Table.Cell width="4">{surgery.date}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell active>Doctor</Table.Cell>
+                        <Table.Cell>{surgery.doctor}</Table.Cell>
+                        <Table.Cell active>Hospital</Table.Cell>
+                        <Table.Cell>{surgery.hospitalName}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell active>Duration</Table.Cell>
+                        <Table.Cell>{surgery.duration}</Table.Cell>
+                        <Table.Cell active>Blockchain Address</Table.Cell>
+                        <Table.Cell>{surgery.blockchainAddress}</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
+            <Card.Description>
+              <Container textAlign="center">
+                <Label size="big" basic pointing='below'>
+                  Extra Information 
+                </Label>
+                <h6>Due to the patient lake of sleep during the last two months and according to the tests he did it appeared to be a problem that needed an argunt  </h6>
+              </Container>
+              <hr/>
+              <Container textAlign="center" style={{paddingBottom: '10px'}}>
+                <Label size="big" basic pointing='below'>
+                  File
+                </Label>
+                <br/>
+                <Image
+                  size="medium"
+                  label={{ as: 'a', corner: 'left', icon: 'plus' }}
+                  src='surgeryDocument.jpg'
+                />
+              </Container>
+            </Card.Description>
+        </Card>
+      );
+    })
+
     return (
       <div>
         <AuthBoilerplate history={this.props.history}>
-          <div>those are the surgeruies</div>
+          <Container padded="true" style={{ padding: "20px" }}>
+            <Segment>
+              <h3>Surgeries for someone</h3>
+            </Segment>
+            <br/>
+            <Card.Group>
+              {surgeriesCards}
+            </Card.Group>
+          </Container>
         </AuthBoilerplate>
       </div>
     )
